57d986b5e99d4865de940d0d74f69deb00ffd1dc
TeofilWislocki <48604688+TeofilWislocki@users.noreply.github.com>
2019-10-30 14:23:01 -0300

    Commit 05
    Commit5 085

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { WelcomeComponent } from './home/welcome.component';
-import { ProductModule } from './products/product.module';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    ProductListComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    RouterModule.forRoot([
-      { path: 'welcome', component: WelcomeComponent },
-      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-    ]),
-    ProductModule
+    BrowserModule
   ],
   bootstrap: [AppComponent]
 })
