449e5bb179111925514e4aae4cc1dd738880d680
TeofilWislocki <48604688+TeofilWislocki@users.noreply.github.com>
2019-10-30 14:24:21 -0300

    Commit 06
    Commmmit 06

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,55 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { IProduct } from './product';
-import { ProductService } from './product.service';
+import { Component } from '@angular/core';
 
 @Component({
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  selector: 'pm-products',
+  templateUrl: './product-list.component.html'
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  errorMessage: string;
-
-  _listFilter: string;
-  get listFilter(): string {
-    return this._listFilter;
-  }
-  set listFilter(value: string) {
-    this._listFilter = value;
-    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
-  }
-
-  filteredProducts: IProduct[];
-  products: IProduct[] = [];
-
-  constructor(private productService: ProductService) {
-
-  }
-
-  onRatingClicked(message: string): void {
-    this.pageTitle = 'Product List: ' + message;
-  }
-
-  performFilter(filterBy: string): IProduct[] {
-    filterBy = filterBy.toLocaleLowerCase();
-    return this.products.filter((product: IProduct) =>
-      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
-  }
+  listFilter: string = 'cart';
+  products: any[] = [
+    {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2019",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "assets/images/garden_cart.png"
+    },
+    {
+      "productId": 5,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2019",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "assets/images/hammer.png"
+    }
+  ];
 
   toggleImage(): void {
     this.showImage = !this.showImage;
-  }
-
-  ngOnInit(): void {
-    this.productService.getProducts().subscribe({
-      next: products => {
-        this.products = products;
-        this.filteredProducts = this.products;
-      },
-      error: err => this.errorMessage = err
-    });
   }
 }
